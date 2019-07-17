e18e8de2e8e5d0459fee5b4a57835692e5540e5b
Mark van der Loo <mark.vanderloo@gmail.com>
2019-07-17 11:25:14 +0200

    added datasets

---
+++ pkg/R/datasets.R
@@ -0,0 +1,61 @@
+#' @name COVdat
+#'
+#' @title Example data sets from the Eurostat Generic Validation Rules
+#' @description
+#' These are the datasets from the 
+#' \href{../doc/20180202_maintypes.pdf}{Eurostat document} where the 
+#' generic validaiton rules are defined. They can be loaded with
+#' \code{data(name)}, for example \code{data(FDLdat)}. The following 
+#' datasets are available:
+#'
+#' \itemize{
+#'    \item{\code{COVdat}}
+#'    \item{\code{FDMdat}}
+#'    \item{\code{REPdat}}
+#'    \item{\code{RNRdat}}
+#'    \item{\code{RWDdat}}
+#'    \item{\code{FDLdat}}
+#'    \item{\code{FDTdat}}
+#'    \item{\code{RETdat}}
+#'    \item{\code{RTSdat}}
+#'    \item{\code{VIRdat}}
+#'}
+#' @docType data
+#' @format csv files.
+{}
+
+#' @name FDMdat
+#' @rdname COVdat
+{}
+
+#' @name REPdat
+#' @rdname COVdat
+{}
+
+#' @name RNRdat
+#' @rdname COVdat
+{}
+
+#' @name RWDdat
+#' @rdname COVdat
+{}
+
+#' @name FDLdat
+#' @rdname COVdat
+{}
+
+#' @name FDTdat
+#' @rdname COVdat
+{}
+
+#' @name RTSdat
+#' @rdname COVdat
+{}
+
+#' @name VIRdat
+#' @rdname COVdat
+{}
+
+#' @name RETdat
+#' @rdname COVdat
+{}
