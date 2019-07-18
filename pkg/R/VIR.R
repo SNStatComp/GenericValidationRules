7f8027afb28380dcfbcc84d49e1e0a7d7b0ec4e4
Mark van der Loo <mark.vanderloo@gmail.com>
2019-07-18 09:56:52 +0200

    examples for VIR

--- pkg/R/VIR.R
+++ pkg/R/VIR.R
@@ -7,15 +7,36 @@
 #' @param Min smallest allowed value
 #' @param Max largest allowed value
 #'
-#'
-#'
-#'
+#' @note
+#' Checking ranges can be doen with \code{VIR} but is even easier to do directly
+#' with \pkg{validate}. See the examples.
 #'
 #' @return A \code{logical} with the length of \code{d}.
 #'
 #' @references 
 #' 
 #' \href{../doc/20180202_maintypes.pdf}{Main types of validation rules for ESS data}: VIR
+#' 
+#' @examples
+#' data(VIRdat)
+#' # Using VIR directly
+#' VIR(VIRdat$OBS_VALUE, Min = 1)
+#' 
+#' # Using VIR in a validation rule
+#' library(validate)
+#' rules <- validator(VIR(OBS_VALUE, Min=1) == TRUE)
+#' cf <- confront(VIRdat, rules)
+#' summary(cf)
+#' as.data.frame(cf)
+#' 
+#' # Using 'validate' directly
+#' rules <- validator(OBS_VALUE >= 1)
+#' cf <- confront(VIRdat, rules)
+#' summary(cf)
+#' as.data.frame(cf)
+#' 
+#' 
+#' 
 #' 
 #' @export
 VIR <- function(d, Min=NULL, Max=NULL){
