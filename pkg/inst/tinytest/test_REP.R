c12ebf8be3ff68f34191c03a34f55a8cb428c4ff
Mark van der Loo <mark.vanderloo@gmail.com>
2019-07-18 17:02:32 +0200

    documented REP, added keyTable argument

--- pkg/inst/tinytest/test_REP.R
+++ pkg/inst/tinytest/test_REP.R
@@ -1,15 +1,29 @@
 library(validate)
-source("../../R/REP.R")
-
+voptions(raise="all")
+#source("pkg/R/REP.R")
 # Test data:
 dat <- read.csv('data/REP.csv')
-ref <- data.frame('ADJUST'=c("S", "N"))
+reftab <- data.frame(ADJUST=c("S", "N"))
 
 # Example in par 4.3.1 of ESTAT doc:
-rule <- validator(REP(coverage="Only",TABLE, FREQ, TIME_PERIOD, REPORTING, PARTNER, DIRECTION, AGE, ADJUST) == TRUE)
-cf <- confront(dat, rule, ref=ref)
+rule <- validator(REP(coverage="Only", keyTable=ref
+    ,TABLE, FREQ, TIME_PERIOD, REPORTING, PARTNER, DIRECTION, AGE, ADJUST) == TRUE)
 
-summary(cf)
+cf <- confront(dat, rule, ref = reftab) 
+
 out <- as.data.frame(cf)
-
 expect_equal(out$value, c(TRUE, TRUE, FALSE))
+
+out <- REP(coverage="Only",keyTable = reftab
+    , TABLE       = dat$TABLE
+    , FREQ        = dat$FREQ
+    , TIME_PERIOD = dat$TIME_PERIOD
+    , REPORTING   = dat$REPORTING
+    , PARTNER     = dat$PARTNER
+    , DIRECTION   = dat$DIRECTION
+    , AGE         = dat$AGE
+    , ADJUST      = dat$ADJUST )
+
+expect_equal(out, c(TRUE, TRUE, FALSE))
+
+
