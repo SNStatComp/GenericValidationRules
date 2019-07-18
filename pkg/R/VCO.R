6ef0cd7c82d1fbefefdd7426d2bf677d8c16d810
Mark van der Loo <mark.vanderloo@gmail.com>
2019-07-18 09:47:16 +0200

    examples for VCO

--- pkg/R/VCO.R
+++ pkg/R/VCO.R
@@ -14,9 +14,9 @@
 #'     \eqn{\ldots},\code{VALN} of this variabe become columns after pivoting.
 #' @param idvars Identifying variables: these variables stay in rows after pivoting. All variables
 #'     that are not pivot or identifying are considered observations. A variable named \code{OBS}
-#'     will be split over columns \code{OBS.VAL1}, \code{OBS.VAL2}, \eqn{\ldots}. When used in
-#'     a validation rule, this must be a literal character. That is, its value can not be
-#'     a variable defined earlier in the script.
+#'     will be split over columns \code{OBS.VAL1}, \code{OBS.VAL2}, \eqn{\ldots}. When \code{VCO} is used in
+#'     a validation rule with \pkg{validate}, \code{idvars} must be a literal character vector. 
+#'     That is, its value can not be a variable defined earlier in the script.
 #' @param rule A bare expression in terms of the \code{OBS.VARi} variables.
 #'
 #'
@@ -28,10 +28,20 @@
 #' 
 #' @examples
 #' data(VCOdat)
-#' 
-#' VCO(VCOdat, pivot="AGE"
-#'  , idvars=c("TABLE","FREQ", "TIME_PERIOD","REPORTING","PARTNER","DIRECTION")
-#'  , rule=OBS_VALUE.Y0_18/OBS_VALUE.TOTAL<0.5) 
+#' # Example using VCO directly on a data set.
+#' VCO(VCOdat, pivot = "AGE"
+#'  , idvars = c("TABLE","FREQ", "TIME_PERIOD","REPORTING","PARTNER","DIRECTION")
+#'  , rule = OBS_VALUE.Y0_18/OBS_VALUE.TOTAL<0.5)
+#'
+#' # Example using VCO in a validation rule with the 'validate' package.
+#' library(validate)
+#' rules <- validator(
+#'   VCO(VCOdat, pivot = "AGE"
+#'    , idvars = c("TABLE","FREQ", "TIME_PERIOD","REPORTING","PARTNER","DIRECTION")
+#'    , rule = OBS_VALUE.Y0_18/OBS_VALUE.TOTAL<0.5) == TRUE)
+#' cf <- confront(VCOdat, rules)
+#' summary(cf)
+#' as.data.frame(cf)
 #'
 #' @export
 VCO <- function(data, pivot, idvars, rule){
